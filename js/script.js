00d046c66180a4efdb1093ff48e79660a805bdca
Salvatore Napoli <snapoli1998@gmail.com>
2021-01-26 15:27:28 +0100

    Aggiunta immagine

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ var app = new Vue(
  		el: '.page-container',
  		data: {
 			message: 'Hello world',
+			imgLink: 'https://image.shutterstock.com/image-vector/document-icon-trendy-flat-style-600w-674478544.jpg',
 		},
 	},
 );
